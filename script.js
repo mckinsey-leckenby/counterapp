ad77e0ac79d830c41436b496deba8bc81e1918a9
mckinseyleckenby <leckenbym11@gmail.com>
2022-02-04 10:41:11 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const count = document.querySelector(".count");
+const add = document.querySelector(".add");
+const resetCount = document.querySelector(".reset");
+const subtract = document.querySelector(".subtract");
+const buttons = document.querySelector(".buttons")
+
+buttons.addEventListener("click", (e) => {
+    if(e.target.classList.contains("add")) {
+        count.innerHTML++;
+        setColor();
+    }
+    if(e.target.classList.contains("reset")) {
+        count.innerHTML = 0;
+        setColor();
+    }
+    if(e.target.classList.contains("subtract")) {
+        count.innerHTML--;
+        setColor();
+    }
+})
+
+function setColor() {
+    if(count.innerHTML > 0) {
+        count.style.color = "yellow"
+    }else if(count.innerHTML < 0){
+        count.style.color = "orangered"
+    }else{
+        count.style.color = "white";
+    }
+}
